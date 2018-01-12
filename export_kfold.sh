6060dd2d82f81d9c878d08788bd234cfbc0d5259
irlyue <1554484645@qq.com>
2018-01-12 14:40:39 +0800

    Bug fix

--- export_kfold.sh
+++ export_kfold.sh
@@ -2,6 +2,8 @@ export MODULE_PATH=/home/wenfeng/models/models/research
 export BASE_DIR=/home/wenfeng/all-files/skin-lesion-seg-v2
 export PYTHONPATH=$PYTHONPATH:$MODULE_PATH:$MODULE_PATH/slim
 
+cd $MODULE_PATH
+
 #####################################for loop#######################################
 for fold in {0..3}
 do
