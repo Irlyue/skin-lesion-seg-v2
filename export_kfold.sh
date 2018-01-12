1dd81c0721836bd5d03fe9b2e16c896d8c26f934
Wenfeng <Irlyue@outlook.com>
2018-01-12 14:35:34 +0800

    Add shell script to export k models

---
+++ export_kfold.sh
@@ -0,0 +1,18 @@
+export MODULE_PATH=/home/wenfeng/models/models/research
+export BASE_DIR=/home/wenfeng/all-files/skin-lesion-seg-v2
+export PYTHONPATH=$PYTHONPATH:$MODULE_PATH:$MODULE_PATH/slim
+
+#####################################for loop#######################################
+for fold in {0..3}
+do
+
+python object_detection/export_inference_graph.py \
+    --input_type=image_tensor \
+    --pipeline_config_path=$BASE_DIR/training/ssd_mobilenet_v1_pets.config \
+    --trained_checkpoint_prefix=$BASE_DIR/training/train/$fold/model.ckpt-2000 \
+    --output_directory=$BASE_DIR/training/train/$fold/output_inference_graph.pb
+
+done
+####################################################################################
+
+exit 0
