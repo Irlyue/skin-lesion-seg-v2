4fc8b69228bab5d65a62cf0fdf11bb852ded9a74
irlyue <1554484645@qq.com>
2018-01-13 12:49:12 +0800

    Add script argument

--- export_kfold.sh
+++ export_kfold.sh
@@ -1,3 +1,14 @@
+if [ "$#" != "1" ]
+then
+	echo "Please enter the checkpoint number"
+	exit 1
+fi
+
+echo "###############################################################################"
+echo "########################   Exporting model-$1  ################################"
+echo "###############################################################################"
+
+
 export MODULE_PATH=/home/wenfeng/models/models/research
 export BASE_DIR=/home/wenfeng/all-files/skin-lesion-seg-v2
 export PYTHONPATH=$PYTHONPATH:$MODULE_PATH:$MODULE_PATH/slim
@@ -13,7 +24,7 @@ echo "-------------------------->Exporting model $fold<-------------------------
 python object_detection/export_inference_graph.py \
     --input_type=image_tensor \
     --pipeline_config_path=$BASE_DIR/training/ssd_mobilenet_v1_pets.config \
-    --trained_checkpoint_prefix=$BASE_DIR/training/train/$fold/model.ckpt-2000 \
+    --trained_checkpoint_prefix=$BASE_DIR/training/train/$fold/model.ckpt-$1 \
     --output_directory=$BASE_DIR/training/train/$fold/output_inference_graph.pb
 
 done
