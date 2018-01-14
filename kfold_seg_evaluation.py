da432c67f37857ddedce2b4aa9a468e7e977f941
Wenfeng <Irlyue@outlook.com>
2018-01-14 13:19:16 +0800

    Code changes for segmentation evaluation

--- kfold_seg_evaluation.py
+++ kfold_seg_evaluation.py
@@ -1,17 +1,47 @@
 import os
 import crf
 import json
-import my_utils
 import inputs
+import my_utils
 import evaluation
+import numpy as np
 import tensorflow as tf
 
 
 logger = my_utils.get_default_logger()
 
 
-def kfold_evaluation():
-    cnns, crfs, crf_labels = [], [], []
+def cnn_eval_one(mm, image, label):
+    out = mm.inference(image, ['mask'])[0]
+    out = np.squeeze(out)
+    result = my_utils.metric_many_from_predictions(predictions=out,
+                                                   labels=label)
+    return result
+
+
+def crf_eval_one(mm, image, label):
+    out, prob = mm.inference(image, ['mask', 'prob'])
+    out, prob = np.squeeze(out), np.squeeze(prob)
+    unary = crf.get_unary_term(prob)
+    crf_out = crf.crf_post_process(image, unary)
+    result = my_utils.metric_many_from_predictions(predictions=crf_out,
+                                                   labels=label)
+    return result
+
+
+def crf_label_eval_one(mm, image, label, gt_prob):
+    out, prob = mm.inference(image, ['mask', 'prob'])
+    out, prob = np.squeeze(out), np.squeeze(prob)
+
+    unary = crf.get_unary_term(out, unary_from='label', gt_prob=gt_prob, n_classes=2)
+    crf_label_out = crf.crf_post_process(image, unary)
+    result = my_utils.metric_many_from_predictions(predictions=crf_label_out,
+                                                   labels=label)
+    return result
+
+
+def kfold_evaluation(eval_one_func):
+    results = []
     logger.info('K-fold evaluation process...')
     config = my_utils.load_config()
     dermquest = inputs.load_training_data('dermquest', config)
@@ -26,17 +56,15 @@ def kfold_evaluation():
                                                      train_dir=os.path.join(config['train_dir'], str(i)))
         logger.info('Evaluating for %i-th fold data...' % i)
         mm = evaluation.SegRestoredModel(tf.train.latest_checkpoint(config['train_dir']))
-        cnn_result, crf_result, crf_label_result = test_one_model(mm, test_data, kfold_config)
-        cnns.append(cnn_result)
-        crfs.append(crf_result)
-        crf_labels.append(crf_label_result)
+        result = test_one_model(mm, test_data, kfold_config, eval_one_func)
+        results.append(result)
         logger.info('************************************\n\n')
     logger.info('Done evaluation')
-    return cnns, crfs, crf_labels
+    return results
 
 
-def test_one_model(model, listing, config, use_bbox=False, gt_prob=0.6):
-    cnn_result = {
+def test_one_model(model, listing, config, eval_one_func):
+    result = {
         'TP': 0,
         'TN': 0,
         'FP': 0,
@@ -47,35 +75,12 @@ def test_one_model(model, listing, config, use_bbox=False, gt_prob=0.6):
         for key in to_update:
             target[key] += to_update[key]
 
-    crf_result = cnn_result.copy()
-    crf_label_result = cnn_result.copy()
     for base in listing:
         image, label, bbox = inputs.load_one_example(base, size=config['input_size'])
-        cnn_out, prob = model.inference(image, ['mask', 'prob'])
-
-        cnn_out, prob = cnn_out[0], prob[0]
-
-        unary = crf.get_unary_term(prob)
-        crf_out = crf.crf_post_process(image, unary)
-
-        unary = crf.get_unary_term(cnn_out, unary_from='label', n_classes=2, gt_prob=gt_prob)
-        crf_label_out = crf.crf_post_process(image, unary)
-
-        cnn_result_i = my_utils.count_many(cnn_out, label)
-        crf_result_i = my_utils.count_many(crf_out, label)
-        crf_label_result_i = my_utils.count_many(crf_label_out, label)
-
-        update_dict(cnn_result, cnn_result_i)
-        update_dict(crf_result, crf_result_i)
-        update_dict(crf_label_result, crf_label_result_i)
-    cnn_result = my_utils.metric_many_from_counter(cnn_result)
-    crf_result = my_utils.metric_many_from_counter(crf_result)
-    crf_label_result = my_utils.metric_many_from_counter(crf_label_result)
-    return {
-        'cnn': cnn_result,
-        'crf': crf_result,
-        'crf_label': crf_label_result
-    }
+        result_i = eval_one_func(model, image, label)
+        update_dict(result, result_i)
+    result = my_utils.metric_many_from_counter(result)
+    return result
 
 
 def aggregate_result(results):
@@ -97,8 +102,22 @@ def aggregate_result(results):
     logger.info('Final result:\n%s' % json.dumps(final_result, indent=2))
 
 
+def mean_aggregate_result(results):
+    final_result = {}
+    for key in results[0]:
+        final_result[key] = np.mean(result[key] for result in results)
+    return final_result
+
+
 if __name__ == '__main__':
-    rs = kfold_evaluation()
-    for k, result in rs:
-        logger.info(k)
-        logger.info('\n%s\n' % json.dumps(result, indent=2))
+    eval_funcs = {
+        'cnn': cnn_eval_one,
+        'crf': crf_eval_one,
+        'crf_label': lambda mm, image, label: crf_label_eval_one(mm, image, label, gt_prob=0.9)
+    }
+    for k, eval_func in eval_funcs.items():
+        logger.info('-------------------------> %s' % k)
+        rs = kfold_evaluation(eval_func)
+        fr = mean_aggregate_result(rs)
+        logger.info('\n'.join('%s' % json.dumps(r, indent=2) for r in rs))
+        logger.info('\n---->Final Result<----\n%s' % json.dumps(fr, indent=2))
